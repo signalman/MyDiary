45103236be45bca2cd432384b18657e6beff0984
hoin <apicad0307@gmail.com>
2022-09-19 05:57:35 +0900

    login by cookie

--- src/main/java/Diary/demo/controller/Home.java
+++ src/main/java/Diary/demo/controller/Home.java
@@ -1,16 +1,16 @@
 package Diary.demo.controller;
 
 import Diary.demo.domain.Member;
-import Diary.demo.repository.MemberRepository;
 import Diary.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.annotation.WebServlet;
-import java.util.Optional;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,12 +18,20 @@ import java.util.Optional;
 public class Home {
     private final MemberService memberService;
 
+    @GetMapping("/")
+    public String hello(@CookieValue(name="memberId", required = false) String memberId, Model model){
+        model.addAttribute("memberId", memberId);
+        return "index";
+    }
+
     @PostMapping("login")
-    public ModelAndView loginComp(@RequestParam String uid, String password) {
-        //로그인을 검증하는 로직?
+    public ModelAndView loginComp(HttpServletResponse response, @RequestParam String uid, String password) {
         ModelAndView mv = new ModelAndView();
         if(memberService.login(uid, password).isPresent()){
-            mv.setViewName("index");
+            Member findMember = memberService.login(uid, password).get();
+            mv.setViewName("redirect:/");
+            Cookie idCookie = new Cookie("memberId", String.valueOf(findMember.getUid()));
+            response.addCookie(idCookie);
         }
         else{
             mv.setViewName("login");
